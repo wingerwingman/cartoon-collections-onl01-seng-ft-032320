9fe524bd00e3119625487d1971044bf3efb68ad4
Justin Winger <justinwinger4@gmail.com>
2020-03-25 20:37:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -36,5 +36,5 @@ end
 def find_the_cheese#(cheddar_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheddar_cheese.detect{|i| i.
+  cheddar_cheese.detect{|i| i == :cheddar }
 end
