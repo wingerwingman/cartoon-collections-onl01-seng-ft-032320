ca20dc402225cfd9401dd46474b26c813f29e8c3
Justin Winger <justinwinger4@gmail.com>
2020-03-25 20:26:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,7 +33,7 @@ def long_planeteer_calls(calls_long)
   
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese#(cheddar_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
